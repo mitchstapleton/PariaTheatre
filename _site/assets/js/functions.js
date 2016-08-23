a374a07b3704424efe312714b828c80fb5e2d411
Mitch Stapleton <mitch.stapleton@gmail.com>
2016-08-23 21:23:28 +0100

    Headroom.js on classes page!
    Just loads of horrible positioning issues to deal with because of it
    now…

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,35 +1,12 @@
+$(document).ready(function(){
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement, {
+    "offset": 75,
+    "tolerance": 10
 
-    
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
-
+});
+// initialise
+headroom.init(); 
+})
