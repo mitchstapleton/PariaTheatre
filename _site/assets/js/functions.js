4e69040b9c95f9896925842b8c81c2856ad3f693
Mitch Stapleton <mitch.stapleton@gmail.com>
2017-10-15 20:24:56 +0100

    Initial commit

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -11,27 +11,4 @@ var headroom  = new Headroom(myElement, {
 headroom.init(); 
 smoothScroll.init();
     
-$('.classes-lockup').flexslider({
-    animation: "fade",
-    slideshow: false,
-    touch: true,
-    customDirectionNav: $(".custom-navigation a"),
-  });
-    
-$(".custom-nav-prev").click(function(){
-    $('.classes-lockup').flexslider('prev')
-    return false;
 });
-
-$('.custom-nav-next').click(function(){
-    $('.classes-lockup').flexslider('next')
-    return false;
-});
-
-$('.classes-hyperlink').click(function(){
-    $('#message-box').empty();
-    var levelString = " " + $(this).attr("data-level") + " ";
-    var fullString = "Hello! I am interested in learning more about Paria Theatre's level" + levelString + "course.";
-    $('#message-box').text(fullString);
-});
-})
